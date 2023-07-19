df3db547af4886614bfcc43d25d9466911bf7b2c
Alfred Chin <alfredchin2005@gmail.com>
2023-07-19 16:32:38 +0800

    Add logout function

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import AuthPage from "./pages/AuthPage";
 import { AuthProvider } from "./components/AuthProvider";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import MainMenuPage from "./pages/MainMenuPage";
+import { getAuth } from "firebase/auth";
+import SectionAuth from "./components/SectionAuth";
 
 function Layout() {
+  const auth = getAuth(
+
+  )
   return (
     <>
-      <Navbar className="pt-3" style={{ background: "#FAF3E9"}}>
-        <Container>
-          <Navbar.Brand style={{ color: "#FF7F50", fontWeight: "bold" }}>BookMyHealth</Navbar.Brand>
-            <Nav>
-                <Nav.Link href='/' className="me-4" style={{ color: "#FF7F50", fontWeight: "500" }}>Check My Booking schedule</Nav.Link>
-                <Button className="px-4 rounded-pill" style={{ background: "transparent", border: "2px solid #FF7F50", color: "#FF7F50", fontWeight: "500" }}>Logout</Button>
-            </Nav>
-        </Container>
-      </Navbar>
+      <SectionAuth>
+        <Navbar className="pt-3" style={{ background: "#FAF3E9"}}>
+          <Container>
+            <Navbar.Brand style={{ color: "#FF7F50", fontWeight: "bold" }}>BookMyHealth</Navbar.Brand>
+              <Nav>
+                  <Nav.Link href='/' className="me-4" style={{ color: "#FF7F50", fontWeight: "500" }}>Check My Booking schedule</Nav.Link>
+                  <Button className="px-4 rounded-pill" style={{ background: "transparent", border: "2px solid #FF7F50", color: "#FF7F50", fontWeight: "500" }} onClick={() => auth.signOut()}>Logout</Button>
+              </Nav>
+          </Container>
+        </Navbar>
+      </SectionAuth>
       <Outlet />
     </>
   )
@@ -23,15 +30,17 @@ function Layout() {
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainMenuPage />} />
-            <Route path="/login" element={<AuthPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <div style={{ backgroundColor: "#FAF3E9",height: "100vh", overflow: "hidden"}}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<MainMenuPage />} />
+              <Route path="/login" element={<AuthPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
   )
 }
--- src/pages/MainMenuPage.jsx
+++ src/pages/MainMenuPage.jsx
@@ -1,7 +1,18 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../components/AuthProvider"
+import { useNavigate } from "react-router-dom"
+
 export default function MainMenuPage() {
-  return (
-    <div>
-      Hello
-    </div>
-  )
-}
+    const { currentUser } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!currentUser) navigate('/login')
+    }, [currentUser, navigate])
+
+    return (
+        <div>
+            Hello
+        </div>
+    )
+    }
