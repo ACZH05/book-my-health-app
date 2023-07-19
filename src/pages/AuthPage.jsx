f16c1c19dc7ddfd0878103b560a32b80ca68fb53
Alfred Chin <alfredchin2005@gmail.com>
2023-07-19 12:28:42 +0800

    Add login function

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,7 @@
 import { Col, Row, Form, Container, Button } from "react-bootstrap";
 import '../style/AuthPage.css'
 import { useContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { AuthContext } from "../components/AuthProvider";
 import axios from "axios";
 
@@ -10,6 +10,7 @@ export default function AuthPage() {
     const [password, setPassword] = useState("")
     const [phoneNumber, setPhoneNumber] = useState("")
     const [active, setActive] = useState(false)
+    const [error, setError] = useState(null)
 
     const auth = getAuth()
     const { currentUser } = useContext(AuthContext)
@@ -25,14 +26,32 @@ export default function AuthPage() {
         }
         catch (error) {
             console.error(error)
+            setError("This email had already registered before.")
         }
 
+    }
+
+    const handleSignIn = async (e) => {
+        e.preventDefault()
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+        }
+        catch (error) {
+            console.error(error)
+            if (error.code === "auth/wrong-password") {
+                setError("Email or password incorrect.")
+            }
+            else if (error.code === "auth/too-many-requests") {
+                setError("Sorry, you have exceeded the maximum number of allowed login attempts. For security purposes, please try again later.")
+            }
+        }
     }
 
     const handleClickSignIn = () => {
         setActive(true)
         setEmail("")
         setPassword("")
+        setError(null)
     }
 
     const handleClickSignUp = () => {
@@ -40,6 +59,7 @@ export default function AuthPage() {
         setEmail("")
         setPassword("")
         setPhoneNumber("")
+        setError(null)
     }
   return (
     <div style={{ backgroundColor: "#FAF3E9", height: "100dvh", overflow: "hidden"}}>
@@ -70,16 +90,22 @@ export default function AuthPage() {
                         <Form.Control type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="mt-4" style={{ width: "100%"}} placeholder="Enter phone number" />
                         <Button type="submit" className="mt-5 border-0 sign-up-button rounded-pill" style={{ width: "100%", backgroundColor: "#FF7F50"}}>Sign Up</Button>
                     </Form>
+                    {error && (
+                        <p className="text-danger">{error}</p>
+                    )}
                 </Container>
             </Col>
             <Col className="p-0">
                 <Container className="px-5 pt-4" style={{ width: "100%"}}>
                         <div style={{ fontSize: 32, fontWeight: "bold", color: "#FF7F50"}}>Sign In</div>
-                        <Form>
+                        <Form onSubmit={handleSignIn}>
                             <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)}  className="mt-5" style={{ width: "100%"}} placeholder="Enter Email" />
                             <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="mt-4" style={{ width: "100%"}} placeholder="Enter password" />
                             <Button type="submit" className="mt-5 border-0 sign-up-button rounded-pill" style={{ width: "100%", backgroundColor: "#FF7F50"}}>Sign In</Button>
                         </Form>
+                        {error && (
+                            <p className="text-danger">{error}</p>
+                        )}
                     </Container>
             </Col>
         </Row>
